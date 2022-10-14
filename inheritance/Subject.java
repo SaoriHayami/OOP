e666ef9c4893d6e088771304f398d6e88c24c969
Joshua Anton Magbanua <100029027+SaoriHayami@users.noreply.github.com>
2022-10-14 23:29:41 +0800

    Add files via upload

---
+++ inheritance/Subject.java
@@ -0,0 +1,20 @@
+
+
+public class Subject {
+    String subject;
+
+    public Subject()
+    {
+
+    }
+
+    public String getSubject()
+    {
+        return subject;
+    }
+
+    public void setSubject(String subject)
+    {
+        this.subject = subject;
+    }
+}
